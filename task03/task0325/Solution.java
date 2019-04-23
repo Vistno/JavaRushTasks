367da3d55a1786433a769018a164adbe301bda0b
Vistno <48941711+Vistno@users.noreply.github.com>
2019-04-23 12:23:40 +0300

    Add files via upload

---
+++ task03/task0325/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task03.task0325;
+
+import java.io.*;
+
+/* 
+Финансовые ожидания
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String buks = reader.readLine();
+        System.out.println("Я буду зарабатывать $" + buks + " в час");//напишите тут ваш код
+    }
+}
