c127272eb93e792879e8eb8a66858e05ec95f777
Vistno <48941711+Vistno@users.noreply.github.com>
2019-05-13 18:08:37 +0300

    Add files via upload

---
+++ task12/task1208/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void print (int a) {
+
+    }
+
+    public static void print (String s, int a) {
+
+    }
+
+    public static void print (String s, int a, int b) {
+
+    }
+
+    public static void print (int a, String s, int b) {
+
+    }
+
+    public static void print (int a, int b, String s) {
+
+    }
+    //Напишите тут ваши методы
+}
